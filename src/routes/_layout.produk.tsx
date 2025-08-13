b26893245d83b95ce2f09e983fa1f81dc457e4cf
hayasehiise <kuronegahiise@gmail.com>
2025-08-14 06:21:14 +0800

    sudah pake tanstack table

--- src/routes/_layout.produk.tsx
+++ src/routes/_layout.produk.tsx
@@ -1,7 +1,12 @@
 import { createFileRoute, useSearch, useNavigate } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from '@tanstack/react-table'
 import { getProdukOption } from '@/query/produk'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 export const Route = createFileRoute('/_layout/produk')({
   component: ProdukPage,
@@ -21,8 +26,8 @@ function ProdukPage() {
     getProdukOption(page, limit, search),
   )
   const [searchData, setSearchData] = useState(search)
-  const [limitValue, setLimitValue] = useState(10)
-  const [pageValue, setPageValue] = useState(1)
+  const [limitValue, setLimitValue] = useState(limit)
+  const [pageValue, setPageValue] = useState(page)
 
   useEffect(() => {
     const handlerSearch = setTimeout(() => {
@@ -30,12 +35,70 @@ function ProdukPage() {
         to: '/produk',
         search: { page: pageValue, limit: limitValue, search: searchData },
       })
-    }, 1000)
+    }, 500)
 
     return () => clearTimeout(handlerSearch)
   }, [pageValue, searchData, limitValue, navigate])
 
+  const columns = useMemo(
+    () => [
+      {
+        header: '#',
+        cell: (info) => {
+          return info.row.index + 1 + (pageValue - 1) * limitValue
+        },
+      },
+      { accessorKey: 'nama', header: 'Nama Produk' },
+      {
+        accessorKey: 'harga',
+        header: 'Harga',
+        cell: (info) => `Rp.${info.getValue().toLocaleString('id-ID')}`,
+      },
+      {
+        accessorKey: 'stock',
+        header: 'Stock',
+        cell: (info) => (
+          <span className={info.getValue() < 5 ? 'text-red-500 font-bold' : ''}>
+            {info.getValue()}
+          </span>
+        ),
+      },
+      {
+        accessorKey: 'status',
+        header: 'Status',
+        cell: (info) => (info.getValue() === 'TERSEDIA' ? 'Tersedia' : 'Habis'),
+      },
+      {
+        accessorKey: 'createdAt',
+        header: 'Tanggal Input Data',
+        cell: (info) =>
+          new Date(info.getValue()).toLocaleDateString('id-ID', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+          }),
+      },
+      {
+        header: 'Aksi',
+        cell: () => (
+          <div className="flex gap-2">
+            <button className="btn btn-xs">+1 stok</button>
+            <button className="btn btn-xs btn-warning">Update</button>
+            <button className="btn btn-xs btn-error">Delete</button>
+          </div>
+        ),
+      },
+    ],
+    [pageValue, limitValue],
+  )
+  const table = useReactTable({
+    data: data?.data || [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
   console.log(data)
+
   if (isLoading) {
     return (
       <div className="flex flex-col w-full px-10 py-5 justify-center items-center">
@@ -58,7 +121,19 @@ function ProdukPage() {
       <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
         <table className="table">
           <thead>
-            <tr>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+            {/* <tr>
               <th></th>
               <th>Nama Produk</th>
               <th>Harga</th>
@@ -66,10 +141,19 @@ function ProdukPage() {
               <th>Status</th>
               <th>Tanggal Input Data</th>
               <th colSpan={3}>Aksi</th>
-            </tr>
+            </tr> */}
           </thead>
           <tbody>
-            {data.data.map((produk, index) => (
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+            {/* {data.data.map((produk, index) => (
               <tr key={produk.id}>
                 <td>{index + 1}</td>
                 <td>{produk.nama}</td>
@@ -87,7 +171,7 @@ function ProdukPage() {
                 <td>Update</td>
                 <td>Delete</td>
               </tr>
-            ))}
+            ))} */}
           </tbody>
         </table>
       </div>
